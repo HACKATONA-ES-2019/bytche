ca6a0a1c28ffea3aaa23882a8f43366384188037
AGES <leonardovizzotto14@gmail.com>
2019-09-15 02:26:06 -0300

    Adcionei tela Contatos

---
+++ hacka-app/src/screens/contatos/Contatos.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Contatos = () => {
+  return (
+    <div className="Main">
+      <h1 className="title">Em caso de emergência</h1>
+      <hr></hr>
+      <ul className="lista">
+        <li>Policia Militar - 190</li>
+        <li>SAMU - 192</li>
+        <li>Corpo de Bombeiros - 193</li>
+        <li>Defesa Civil - 199</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Contatos;
