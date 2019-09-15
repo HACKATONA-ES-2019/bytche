84254512278b13925a2e992c077446d032f22e60
Bianca Camargo <bianca.camargo@acad.pucrs.br>
2019-09-14 22:02:51 -0300

    screens

--- hacka-app/src/App.js
+++ hacka-app/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Incendio from "./screens/emergencia/Incendio";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Incendio />
       </header>
     </div>
   );
---
+++ hacka-app/src/screens/emergencia/Incendio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Incendio = () => {
+  return (
+    <div className="Main">
+      <p>INCENDIO!!</p>
+    </div>
+  );
+}
+
+export default Incendio;
