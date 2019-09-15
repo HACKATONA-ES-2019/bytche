3c687542fc98994848ebfa91cd1bf0a6dc2991e6
Bianca Camargo <bianca.camargo@acad.pucrs.br>
2019-09-15 05:00:53 -0300

    fix map style

--- hacka-app/src/screens/maps/Maps.js
+++ hacka-app/src/screens/maps/Maps.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import "./maps.css";
  
 export class MapContainer extends React.Component {
   render() {
       const { google } = this.props;
     return (
-      <Map google={google} zoom={14}>
+      <Map 
+        google={google} 
+        zoom={14}
+        className="mapContainer"
+      >
         <Marker 
             onClick={this.onMarkerClick}
             name={'Current location'}
