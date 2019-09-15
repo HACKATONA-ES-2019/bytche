a11f547b4bf2940fae01a3c1d30b684c8d070b6d
Bianca Camargo <bianca.camargo@acad.pucrs.br>
2019-09-14 23:14:49 -0300

    add menu

--- hacka-app/src/App.js
+++ hacka-app/src/App.js
@@ -1,28 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
+import { Phone, Waves, Fireplace, MyLocation } from '@material-ui/icons';
 import './App.css';
 import Incendio from "./screens/emergencia/Incendio";
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Incendio />
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { page: 0 }
+  }
+
+  handlePage = page => {
+    this.setState({ page });
+  };
+
+  render() {
+    const { page } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* adicionar infos */}
+          <div>
+        { page == 0 && <Incendio />}
+        { page == 1 && <Incendio />}
+        { page == 2 && <Incendio />}
+        { page == 3 && <Incendio />}
+
+        </div>
+        </header>
+        <BottomNavigation
+            value={page}
+            onChange={(event, page) => {
+              this.handlePage(page);
+            }}
+            showLabels
+            className={{ width: "100%" }}
+          >
+            <BottomNavigationAction label="Caso de incêndio" icon={<Fireplace />} />
+            <BottomNavigationAction label="Inundação" icon={<Waves />} />
+            <BottomNavigationAction label="Contatos" icon={<Phone />} />
+            <BottomNavigationAction label="Adicionar Locais" icon={<MyLocation />} />
+          </BottomNavigation>
+      </div>
+    );
+  }
 }
 
 export default App;
